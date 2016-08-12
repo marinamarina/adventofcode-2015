c32c5a5d9e53963f6cd34461991cca6d584f9923
Marina Shchukina <shchukina.marina@gmail.com>
2016-08-12 17:43:10 +0100

    Day08 skeleton

---
+++ day08/library.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _ = require('lodash'),
+    fs = require('fs');
+
+module.exports = {
+};
---
+++ day08/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var os = require('os');
+var lib = require('./library')

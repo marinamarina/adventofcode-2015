cdf32707d825e5a202d1675f1ec44866ab2e073b
Marina Shchukina <shchukina.marina@gmail.com>
2016-08-14 16:48:51 +0100

    Day08 - part1

--- day08/solution.js
+++ day08/solution.js
@@ -1,12 +1,21 @@
 'use strict';
-
 var _ = require('lodash'),
     fs = require('fs');
 
 module.exports = {
-    part1: function() {
-        return 'one';
+    part1: (input) => {
+        // 1342
+        let i = input.split('\n');
+        let unescapedArray = i.map((v) => { return v.replace(/\"/g, ""); });
+
+        let rawStringLength = i.join("").length;
+        let escapedStringLength = i.map((v) => {
+            return v.replace(/\"/g, "").replace(/\\x[a-z0-9]{2}|\\"|\\\\/g, '*');
+         }).join('').length + 2;
+
+        return rawStringLength - escapedStringLength;
     },
+
     part2: function() {
         return 'two';
     }

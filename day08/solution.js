3a0754cc162c79b5cae32831339227c39e5dd3da
Marina Shchukina <shchukina.marina@gmail.com>
2016-08-15 21:52:54 +0100

    Day08 - part2

--- day08/solution.js
+++ day08/solution.js
@@ -6,12 +6,22 @@ module.exports = {
     part1: i => {
         //1342
         let rawStringLength = i.join("").length;
-        let escapedStringLength = i.map(v =>v.replace(/\\x[a-z0-9]{2}|\\"|\\\\/g, '*').replace(/\"/g, "")).join('').length;
+        let escapedStringLength = i.map(
+            v => v.replace(/\\x[a-z0-9]{2}|\\"|\\\\/g, '*')
+            .replace(/\"/g, "")
+            ).join('').length;
 
         return rawStringLength - escapedStringLength;
     },
 
-    part2: function() {
-        return 'two';
+    part2: i => {
+        let rawStringLength = i.join("").length;
+        let encodedStringLength = i.map(
+            v => {
+                return v.replace(/\\|\"/g, s => {return '\\' + s;});
+            }
+        ).join('').length + i.length * 2;
+
+        return encodedStringLength - rawStringLength;
     }
 };

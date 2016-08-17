f956f858b8bc34208a736755beb50a69f908f8cb
Marina Shchukina <shchukina.marina@gmail.com>
2016-08-17 23:05:22 +0100

    Day09 skeleton

---
+++ day09/solution.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    part1: i => {
+        const places = new Set();
+        places.add(1);
+        places.add(2);
+        places.add(5);
+        console.log(places);
+
+        return 1;
+    },
+
+    part2: i => {
+
+        return 2;
+    }
+};

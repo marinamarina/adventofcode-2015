0be186472ecb4422cf8e19d935ee8a743c7c1be1
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-01 17:11:28 +0100

    Much cleaner solution for day 09.

--- day09/solution.js
+++ day09/solution.js
@@ -2,56 +2,52 @@
 const Combinatorics = require('js-combinatorics');
 const _ = require('lodash');
 
-const places = new Set();
-const distances = new Map();
+function calculateDistance(i) {
+    const places = new Set(),
+            distances = new Map();
 
-let shortestDistance = Number.MAX_SAFE_INTEGER;
-let longestDistance = 0;
+    let min = Number.MAX_SAFE_INTEGER,
+        max = 0;
 
+    for (let l of i) {
+        let x = l.split(' ');
+        let distance = x[x.length-1];
 
+        // add places to a set
+        places.add(x[0]);
+        places.add(x[2]);
+
+        // create a key-value pair
+        distances.set(`${x[0]},${x[2]}`, parseInt(x[x.length-1]));
+        distances.set(`${x[2]},${x[0]}`, parseInt(x[x.length-1]));
+    };
+
+    let cmb = Combinatorics.permutation(Array.from(places), places.size).toArray();
+
+    _.map(cmb, function(v, i) {
+        let pair1 = _.take(v, 2);
+        let pair2 = v.slice(1,3);
+
+        let d = 0;
+        for (let i=0; i<v.length-1; i++) {
+            d += distances.get(`${v[i]},${v[i+1]}`);
+        }
+        if (d < min) min = d;
+        if (d > max) max = d;
+            return d;
+    });
+
+    return [min, max];
+};
 
 module.exports = {
     part1: i => {
-        for (let line of i) {
-    let lineSplitted = line.split(" ");
-    let place1 = lineSplitted[0];
-    let place2 = lineSplitted[2];
-    let distanceBetween = lineSplitted[lineSplitted.length-1];
-
-    // count how many unique places
-    places.add(place1);
-    places.add(place2);
-
-    // create a key-value pair
-    distances.set(`${place1},${place2}`, parseInt(distanceBetween));
-    distances.set(`${place2},${place1}`, parseInt(distanceBetween));
-
-};
-
-let cmb = Combinatorics.permutation(Array.from(places), places.size).toArray();
-
-_.map(cmb, function(val, idx) {
-    let pair1 = val.slice(0,2);
-    let pair2 = val.slice(1,3);
-
-    let d = 0;
-    for (let i=0; i<val.length-1; i++) {
-        d += distances.get(`${val[i]},${val[i+1]}`);
-    }
-        if (d < shortestDistance) shortestDistance = d;
-        if (d > longestDistance) longestDistance = d;
-    return d;
-
-    });
-        return shortestDistance;
+        let [min, ] = calculateDistance(i);
+        return min;
     },
 
     part2: i => {
-        // destructuring
-        const [a, b] = function foo(){
-            return ['hello', 'world'];
-        }();
-        console.log(a, b);
-        return longestDistance;
+        let [, max] = calculateDistance(i);
+        return max;
     }
 };

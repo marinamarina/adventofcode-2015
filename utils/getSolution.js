d5cd652517de791a484cb69c7aa4b07646f2f02a
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-01 17:25:58 +0100

    Renamed folders and changed getSolution function accordingly.

--- utils/getSolution.js
+++ utils/getSolution.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var path = require('path');
 
 function getSolution (day, part) {
-    var solution = require(path.join(__dirname, `../day0${day}/solution`));
-    var input = fs.readFileSync(path.join(__dirname, `../day0${day}/input.txt`), 'utf8').trim().split('\n');
+    var solution = require(path.join(__dirname, `../day${day}/solution`));
+    var input = fs.readFileSync(path.join(__dirname, `../day${day}/input.txt`), 'utf8').trim().split('\n');
 
     return solution[`part${part}`](input);
 };

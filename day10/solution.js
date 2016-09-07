59f52b5cb17a67727d9e73730271c539c4dfb90a
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-07 23:14:39 +0100

    Day 10-1 look-and-say sequence

--- day10/solution.js
+++ day10/solution.js
@@ -3,7 +3,10 @@ const _ = require('lodash');
 
 module.exports = {
     part1: i => {
-        return 1;
+
+        var parse = input => _.chain( input.match(/(\d)\1*/g) ).map(function(v) { return `${v.length}${v[0]}`; }).value().join('');
+
+        return parse(i[0]);
     },
 
     part2: i => {
